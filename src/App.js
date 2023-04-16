c2a66b9d2761bd45e6979f697336a8b8ae6c85e8
islam231bi <islambassuni@gmail.com>
2023-04-16 16:22:42 +0200

    Added more setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import "./App.css";
-
-import React from "react";
+import React from 'react';
+import './App.css';
+import Board from './components/Board';
+import Options from './components/Options';
+import { Box, Stack } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      Islam
+      <Stack direction="row" spacing={2} >
+        <h1 className="title">8-Puzzle</h1>
+        <Board />
+        <Box>
+          <Options />
+        </Box>
+      </Stack>
+      
     </div>
   );
 }
