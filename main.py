4ff9c18aa27ca7058f610e62a7ec796f38beb240
islam231bi <islambassuni@gmail.com>
2023-04-17 16:30:06 +0200

    Added AManhattan initial implementation

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from queue import PriorityQueue
+
+
 def BFS (initial_state):
     cost = 0
     nodes_expanded = 0
@@ -23,11 +26,53 @@ def AManhattan (initial_state):
     running_time = 0
     goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
+    # Define the node class to store the state, parent node, and cost
+    class Node:
+        def __init__(self, state, parent=None, cost=0):
+            self.state = state
+            self.parent = parent
+            self.cost = cost
 
+        def __lt__(self, other):
+            return self.cost + computeHeuristic(self.state, 'M') < other.cost + computeHeuristic(other.state, 'M')
 
+    # Initialize the frontier with the initial node
+    frontier = PriorityQueue()
+    frontier.put(Node(initial_state))
 
+    # Initialize the explored set
+    explored = set()
+
+    # Loop until the frontier is empty
+    while not frontier.empty():
+        # Get the node with the lowest cost
+        node = frontier.get()
+
+        # Check if the node is the goal state
+        if node.state == goal_state:
+            # If it is, trace back to the root node and return the path
+            path = []
+            while node.parent is not None:
+                path.append(node.state)
+                node = node.parent
+            path.append(initial_state)
+            return list(reversed(path))
+
+        # Add the node to the explored set
+        explored.add(tuple(node.state))
+
+        # Generate the child nodes and add them to the frontier
+        blank_index = node.state.index(0)
+        for action in [-3, -1, 1, 3]:
+            new_index = blank_index + action
+            if 0 <= new_index < 9 and (new_index // 3 == blank_index // 3 or abs(new_index - blank_index) == 1):
+                new_state = node.state[:]
+                new_state[blank_index], new_state[new_index] = new_state[new_index], new_state[blank_index]
+                if tuple(new_state) not in explored:
+                    new_node = Node(new_state, parent=node, cost=node.cost + 1)
+                    frontier.put(new_node)
    
-    return [cost, nodes_expanded, search_depth, running_time]
+    # return [cost, nodes_expanded, search_depth, running_time]
 
 def AEuclidean (initial_state):
     cost = 0
@@ -97,7 +142,9 @@ if __name__ == '__main__':
     else:
         print("invalid choice")
 
-    print("cost: ", output[0])
-    print("nodes expanded: ", output[1])
-    print("search depth: ", output[2])
-    print("running time: ", output[3])
+    # print("cost: ", output[0])
+    # print("nodes expanded: ", output[1])
+    # print("search depth: ", output[2])
+    # print("running time: ", output[3])
+
+    print(output)
