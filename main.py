d9cf8373e310871288f57169376a65f4351c2c0c
Abdullah J Mubarak <es-abdallah.mubarak2024@alexu.edu.eg>
2023-04-18 22:42:00 +0200

    add goal test to your Node structure (class)

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def BFS (initial_state):
     
     return [cost, nodes_expanded, search_depth, running_time]
 
+# add it to your node structure
 def goalTest(state):
     if state == [0, 1, 2, 3, 4, 5, 6, 7, 8]:
         return True
