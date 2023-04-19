724296cf1cfd4f5c33e7f63682ba49ccb1af24d6
islam231bi <islambassuni@gmail.com>
2023-04-19 06:01:43 +0200

    added finding neighbors functions

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from queue import PriorityQueue
-import queue
+import heapq as hq
+
 
 def BFS (initial_state):
     cost = 0
     nodes_expanded = 0
     search_depth = 0
     running_time = 0
-    goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     
     return [cost, nodes_expanded, search_depth, running_time]
 
@@ -15,104 +14,37 @@ def goalTest(state):
     if state == [0, 1, 2, 3, 4, 5, 6, 7, 8]:
         return True
     return False
-
-def DFS(initial_state):
-    frontier = queue.LifoQueue()
-    frontier.put(initial_state)
-    frontier_config = {}
-    frontier_config[tuple(initial_state.config)] = True
-    explored = set()
-    nodes_expanded = 0
-    max_search_depth = 0
-
-    while not frontier.empty():
-        state = frontier.get()
-        print("****** State ******")
-        state.display()
-        explored.add(state.config)
-        if state.goalTest():
-            return (state,nodes_expanded,max_search_depth)
-        
-        nodes_expanded += 1
-        for neighbor in state.expand():
-            if neighbor.config not in explored and tuple(neighbor.config) not in frontier_config:   
-                frontier.put(neighbor)
-                frontier_config[tuple(neighbor.config)] = True
-                if neighbor.cost > max_search_depth:
-                    max_search_depth = neighbor.cost
-    return None
-
-# def DFS (initial_state):
-#     cost = 0
-#     nodes_expanded = 0
-#     search_depth = 0
-#     running_time = 0
-#     goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     
-#     return [cost, nodes_expanded, search_depth, running_time]
+
+def DFS (initial_state):
+    cost = 0
+    nodes_expanded = 0
+    search_depth = 0
+    running_time = 0
+    
+    return [cost, nodes_expanded, search_depth, running_time]
 
 def AManhattan (initial_state):
     cost = 0
     nodes_expanded = 0
     search_depth = 0
     running_time = 0
-    goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-    # Define the node class to store the state, parent node, and cost
-    class Node:
-        def __init__(self, state, parent=None, cost=0):
-            self.state = state
-            self.parent = parent
-            self.cost = cost
+    
 
-        def __lt__(self, other):
-            return self.cost + computeHeuristic(self.state, 'M') < other.cost + computeHeuristic(other.state, 'M')
 
-    # Initialize the frontier with the initial node
-    frontier = PriorityQueue()
-    frontier.put(Node(initial_state))
 
-    # Initialize the explored set
-    explored = set()
-
-    # Loop until the frontier is empty
-    while not frontier.empty():
-        # Get the node with the lowest cost
-        node = frontier.get()
-
-        # Check if the node is the goal state
-        if node.state == goal_state:
-            # If it is, trace back to the root node and return the path
-            path = []
-            while node.parent is not None:
-                path.append(node.state)
-                node = node.parent
-            path.append(initial_state)
-            return list(reversed(path))
-
-        # Add the node to the explored set
-        explored.add(tuple(node.state))
-
-        # Generate the child nodes and add them to the frontier
-        blank_index = node.state.index(0)
-        for action in [-3, -1, 1, 3]:
-            new_index = blank_index + action
-            if 0 <= new_index < 9 and (new_index // 3 == blank_index // 3 or abs(new_index - blank_index) == 1):
-                new_state = node.state[:]
-                new_state[blank_index], new_state[new_index] = new_state[new_index], new_state[blank_index]
-                if tuple(new_state) not in explored:
-                    new_node = Node(new_state, parent=node, cost=node.cost + 1)
-                    frontier.put(new_node)
+    
    
-    # return [cost, nodes_expanded, search_depth, running_time]
+    return [cost, nodes_expanded, search_depth, running_time]
 
 def AEuclidean (initial_state):
     cost = 0
     nodes_expanded = 0
     search_depth = 0
     running_time = 0
-    goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    
+
+
     return [cost, nodes_expanded, search_depth, running_time]
 
 
@@ -148,7 +80,50 @@ def checlSolvable(state):
                 invCount+=1
     return (invCount % 2 == 0)
 
+def findNeighbours(state):
+    res = []
+    ind = state.index(0)
+    if not top(ind):
+        res.append(up_neig(state, ind))
+    if not bottom(ind):
+        res.append(down_neig(state, ind))
+    if not left(ind):
+        res.append(left_neig(state, ind))
+    if not right(ind):
+        res.append(right_neig(state, ind))
+    return res
 
+def top(ind):
+    return ind < 3
+
+def bottom(ind):
+    return ind > 5
+
+def left(ind):
+    return ind in [0, 3, 6]
+
+def right(ind):
+    return ind in [2, 5, 8]
+
+def up_neig(p, ind):
+    _p = list(p)
+    _p[ind], _p[ind-3] = _p[ind-3], _p[ind]
+    return tuple(_p)
+
+def down_neig(p, ind):
+    _p = list(p)
+    _p[ind], _p[ind+3] = _p[ind+3], _p[ind]    
+    return tuple(_p)
+
+def left_neig(p, ind):
+    _p = list(p)
+    _p[ind], _p[ind-1] = _p[ind-1], _p[ind]
+    return tuple(_p)
+
+def right_neig(p, ind):
+    _p = list(p)
+    _p[ind], _p[ind+1] = _p[ind+1], _p[ind]
+    return tuple(_p)
 
 
 if __name__ == '__main__':
@@ -174,9 +149,8 @@ if __name__ == '__main__':
     else:
         print("invalid choice")
 
-    # print("cost: ", output[0])
-    # print("nodes expanded: ", output[1])
-    # print("search depth: ", output[2])
-    # print("running time: ", output[3])
+    print("cost: ", output[0])
+    print("nodes expanded: ", output[1])
+    print("search depth: ", output[2])
+    print("running time: ", output[3])
 
-    print(output)
